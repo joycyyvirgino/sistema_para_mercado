da650a0e76904ed1766f8ba31fa224fe0b23f774
Joycy Virgino <143227567+joycyyvirgino@users.noreply.github.com>
2026-02-11 21:22:40 -0300

    Remove database credentials from ConexaoBD
    Removed sensitive database credentials from the code.

--- dao/ConexaoBD.java
+++ dao/ConexaoBD.java
@@ -6,8 +6,7 @@ import java.sql.SQLException;
 
 public class ConexaoBD {
     private static final String URL ="jdbc:mysql://localhost:3306/mercado";
-    private static final String USUARIO = "root";
-    private static final String SENHA = "Xoy0101+";
+   
 
     public static Connection getConexao() {
         try {
